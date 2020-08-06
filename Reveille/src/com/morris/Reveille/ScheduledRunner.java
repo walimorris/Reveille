af7738c214eadbe49e690886abe3f38587384107
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-08-06 02:29:15 -0700

    Update ScheduledRunner.java

--- Reveille/src/com/morris/Reveille/ScheduledRunner.java
+++ Reveille/src/com/morris/Reveille/ScheduledRunner.java
@@ -21,16 +21,16 @@ public class ScheduledRunner {
 
     public void reveille() {
         Long currentTime = new Date().getTime();
-        if (calendar.getTime().getTime() < currentTime) {
-            calendar.add(Calendar.DATE, 1);
+        if (this.calendar.getTime().getTime() < currentTime) {
+            this.calendar.add(Calendar.DATE, 1);
         }
-        Long startScheduler = calendar.getTime().getTime() - currentTime;
+        Long startScheduler = this.calendar.getTime().getTime() - currentTime;
 
-        calendar.set(Calendar.HOUR, 5);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.AM_PM, Calendar.PM);
+        this.calendar.set(Calendar.HOUR, 5);
+        this.calendar.set(Calendar.MINUTE, 0);
+        this.calendar.set(Calendar.AM_PM, Calendar.PM);
 
-        Long stopScheduler = calendar.getTime().getTime() - currentTime;
+        Long stopScheduler = this.calendar.getTime().getTime() - currentTime;
 
         Runnable task = new Runnable() {
             @Override
